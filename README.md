3d8d3edb3a0181185c7a1b726867f2e478416be5
Alessandro Gnoli <ale.gnoli@raise.com>
2017-04-04 01:32:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nutella image
+
+**WORK IN PROGRESS**
+
+This repo contains a [Packer](https://packer.io) file that describes an image that contains nutella and all of its dependecies correctly configured. The image comes in several flavors using different builders:
+- 
+
+The repo also contains a vagrant file used for testing
+
+
+## TODOs
+- Figure out why checkup fails the first time
+- Figure out broker config for nutella (manual step after DNS?)
+- Fix git version bug
---
+++ scripts/install_nutella.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Installing mongo"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo mv /tmp/mongodb.service /etc/systemd/system/mongodb.service
+sudo systemctl unmask mongodb
+sudo systemctl start mongodb
+
+echo "Installing node"
+sudo apt-get install build-essential libssl-dev -y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install node
+nvm use node
+
+echo "Installing mosca"
+npm install mosca bunyan -g
+echo "alias start-mosca=\"mosca --disable-stats --http-port 1884 > /dev/null 2>&1 &\"" >> ~/.bash_profile
+echo "alias stop-mosca=\"kill $(pgrep -f mosca)\"" >> ~/.bash_profile
+source ~/.bash_profile
+start-mosca
+
+echo "Installing rvm and Ruby"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.4
+source /home/ubuntu/.rvm/scripts/rvm
+
+echo "Installing nutella"
+gem install nutella_framework --no-ri --no-rdoc
+# nutella checkup
+
+# WORK IN PROGRESS
+
+# apt-get upgrade and security-update
+# restart
